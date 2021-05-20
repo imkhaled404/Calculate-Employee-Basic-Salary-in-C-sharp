08dcb05c83f4c6308854500cfcb83de98faa44b1
KhaledMasud Babu <31078651+imkhaled404@users.noreply.github.com>
2021-05-21 00:34:06 +0600

    Add files via upload

---
+++ basicSalary/basicSalary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicSalary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            doSalary2();
+            Console.ReadKey();
+        }
+
+        static void doSalary2()
+        {
+            Console.WriteLine("Input Basic Salary");
+            var bs = Console.ReadLine();
+            Salary s = new Salary();
+            var bsReturn = s.CalculateByBasicSalary(Convert.ToInt32(bs));
+            Console.WriteLine(">> Basic Salary:         {0}/= {1}", s.basicSalary, s.currency);
+            Console.WriteLine(">> House Rent:           {0}/= {1}", s.houseRent, s.currency);
+            Console.WriteLine(">> Medical Allowance:    {0}/= {1}", s.medicalAllowance, s.currency);
+            Console.WriteLine(">> Conveyance Allowance: {0}/= {1}", s.conveyanceAllowance, s.currency);
+            Console.WriteLine("......................................");
+            Console.WriteLine("   Gross Salary:         {0}/= {1}", bsReturn, s.currency);
+        }
+    }
+}
---
+++ basicSalary/basicSalary/Salary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicSalary
+{
+    class Salary
+    {
+        public int basicSalary;
+        public int houseRent;
+        public int medicalAllowance;
+        public int conveyanceAllowance;
+        public Currency currency;
+
+        double bs_percent;
+        double hr_percent;
+        double ma_percent;
+        double ca_percent;
+
+
+        ///-----------Gross Salary Calculate---------/
+        public int CalculateByBasicSalary(int basicSalary)
+        {
+            this.bs_percent = 0.50; // 50% basic
+            this.hr_percent = 0.30; // 30% house
+            this.ma_percent = 0.10; // 10% medical
+            this.ca_percent = 0.10; // 10% conveyance
+
+            //-------------------------------
+            // Basic                 100%
+            this.currency = Currency.BDT;
+
+            this.basicSalary = basicSalary;
+            this.houseRent = Convert.ToInt32(basicSalary * 0.30);
+            this.medicalAllowance = Convert.ToInt32(basicSalary * 0.10);
+            this.conveyanceAllowance = Convert.ToInt32(basicSalary * 0.10);
+            var re = this.basicSalary + this.houseRent + this.medicalAllowance + this.conveyanceAllowance;
+            return re;
+        }
+
+    }
+
+    public enum Currency
+    {
+        BDT, RS, USD, ERO, CNY, YEN
+    }
+}
